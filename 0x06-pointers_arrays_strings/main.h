04fc327b87be249634b094a99e1bb2703802d5dc
Fummex1987 <onasanyafunmilayo22@gmail.com>
2023-07-27 06:17:48 -0700

    rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,10 @@
 #include <string.h>
 
 
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
