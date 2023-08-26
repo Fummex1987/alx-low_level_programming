d71ddf04ce15174b394bf80c3a438ae2bc94982d
Fummex1987 <onasanyafunmilayo22@gmail.com>
2023-08-26 00:55:55 -0700

    all_element

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
@@ -26,7 +25,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
-
-
 
 #endif
